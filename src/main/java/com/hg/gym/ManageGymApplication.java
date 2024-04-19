d69ea3074fcf9a28e48beacee5fda479dc687ddf
helloHarsh123 <hagu0987@gmail.com>
2024-04-20 00:27:12 +0530

    Initialised Spring Project

---
+++ src/main/java/com/hg/gym/ManageGymApplication.java
@@ -0,0 +1,13 @@
+package com.hg.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hg/gym/ManageGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
